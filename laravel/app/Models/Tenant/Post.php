f809130b47319c29ec3558ab76bac1e795f18eca
SuperDJ <6484766+SuperDJ@users.noreply.github.com>
2025-04-02 11:11:23 +0000

    Add model events broadcasting

--- laravel/app/Models/Tenant/Post.php
+++ laravel/app/Models/Tenant/Post.php
@@ -2,9 +2,28 @@
 
 namespace App\Models\Tenant;
 
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Database\Eloquent\BroadcastsEvents;
+use Illuminate\Database\Eloquent\Model;
+
 class Post extends Model
 {
+    use BroadcastsEvents, HasFactory;
+
     protected $connection = 'tenant';
 
     protected $fillable = ['title', 'description'];
+
+    /**
+     * Get the channels that model events should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel|\Illuminate\Database\Eloquent\Model>
+     */
+    public function broadcastOn(string $event): array
+    {
+        return [
+            new PrivateChannel( 'posts.'.$this->id ),
+        ];
+    }
 }
